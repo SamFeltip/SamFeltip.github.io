c210f2f2d8e016443ff36b929ca4468e91048e97
Samuel Felton <78095426+SamFeltip@users.noreply.github.com>
2023-06-19 15:00:02 +0100

    added carousel, planetgo page is now styled with the projects theme

---
+++ js/carousel.js
@@ -0,0 +1,55 @@
+let carousels = document.querySelectorAll(".carousel");
+
+let carousel_prev_options = document.querySelectorAll(".prev-option");
+let carousel_next_options = document.querySelectorAll(".next-option");
+
+// give every carousel a default state
+carousels.forEach(carousel => {carousel.style.setProperty('--position-state', 0)})
+
+function change_carousel_display(carousel, current_carousel_state, new_carousel_state){
+
+    // get carousel parent
+
+    let imgs = carousel.querySelectorAll('img');
+    let descriptions = carousel.querySelectorAll('.carousel-description');
+
+    let carousel_bullets = carousel.querySelectorAll('.carousel-bullet');
+
+    // hide current images and descriptions
+    imgs[current_carousel_state].classList.remove('display');
+    descriptions[current_carousel_state].classList.remove('display');
+    carousel_bullets[current_carousel_state].classList.remove('filled');
+
+    // update carousel
+    carousel.style.setProperty('--position-state', new_carousel_state);
+
+    // reveal new image and description
+    imgs[new_carousel_state].classList.add('display');
+    descriptions[new_carousel_state].classList.add('display');
+    carousel_bullets[new_carousel_state].classList.add('filled');
+}
+
+carousel_next_options.forEach( (carousel_next_option) => {
+    carousel_next_option.addEventListener("click", event => {
+        let carousel = carousel_next_option.closest('.carousel');
+        let current_carousel_state = carousel.style.getPropertyValue('--position-state')
+        let new_carousel_state = (current_carousel_state + 1) % 3;
+
+        change_carousel_display(carousel, current_carousel_state, new_carousel_state)
+
+    })
+})
+
+carousel_prev_options.forEach( (carousel_prev_option) => {
+
+    carousel_prev_option.addEventListener("click", event => {
+        let carousel = carousel_prev_option.closest('.carousel');
+        let current_carousel_state = parseInt(carousel.style.getPropertyValue('--position-state'));
+
+        let new_carousel_state = current_carousel_state === 0 ? 2 : current_carousel_state - 1;
+
+
+        change_carousel_display(carousel, current_carousel_state, new_carousel_state)
+
+    })
+})
--- js/planetGO.js
+++ js/planetGO.js
@@ -1,20 +1,36 @@
 
-let des_body = document.querySelector("#designer_body");
-let dev_body = document.querySelector("#developer_body");
+let page_tab_links = document.querySelector(".page-tabs");
 
-let designing_button = document.querySelector("#designing");
-let developing_button = document.querySelector("#developing");
+let des_body = document.querySelector("#design-body");
+let dev_body = document.querySelector("#develop-body");
+let plan_body = document.querySelector("#plan-body");
+
+let designing_button = page_tab_links.querySelector("#designing");
+let developing_button = page_tab_links.querySelector("#developing");
+let planning_button = page_tab_links.querySelector("#planning");
+
+planning_button.addEventListener("click", event => {
+  let body = document.querySelector("body");
+
+  des_body.style.display = "none";
+  dev_body.style.display = "none";
+  plan_body.style.display = "flex";
+
+  designing_button.classList.remove("selected");
+  developing_button.classList.remove("selected");
+  planning_button.classList.add("selected");
+
+});
 
 developing_button.addEventListener("click", event => {
   let body = document.querySelector("body");
-  body.classList.remove("light");
-  body.classList.add("dark");
 
   des_body.style.display = "none";
+  plan_body.style.display = "none";
   dev_body.style.display = "flex";
 
-
   designing_button.classList.remove("selected");
+  planning_button.classList.remove("selected");
   developing_button.classList.add("selected");
 
 });
@@ -25,9 +41,11 @@ designing_button.addEventListener("click", event => {
   body.classList.add("light");
 
   dev_body.style.display = "none";
+  plan_body.style.display = "none";
   des_body.style.display = "flex";
 
-  designing_button.classList.add("selected");
+  planning_button.classList.remove("selected");
   developing_button.classList.remove("selected");
+  designing_button.classList.add("selected");
 
 });
